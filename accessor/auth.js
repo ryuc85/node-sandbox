03c1f5ab242d807ae8171db8e329d6d972e453e0
Ryuc85 <marshorizon2015@gmail.com>
2018-01-28 00:30:16 +0900

    Add the md5 hasher and tester.

---
+++ accessor/auth.js
@@ -0,0 +1,6 @@
+var pgp = require('pg-promise')(/*options*/);
+var db = pgp('postgres://pehvgufyzhacll:ff327a12708bbfe5dace30199bc0dae071ee3a3846571ee4074e8da65352817c@ec2-54-163-228-190.compute-1.amazonaws.com:5432/denajo9ommrpca');
+
+exports.insert_user = function (name, passwd, age) {
+    
+}
